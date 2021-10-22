d7a55ba66eeb3ef2b2e7e2eeba419a49bf0b90d8
Maxim Van de Wynckel <maxim.van.de.wynckel@vub.be>
2021-10-22 11:22:41 +0200

    ci: use docker

---
+++ docker/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+tag=node$3-opencv$1
+with_contrib=""
+if [ "$2" == "y" ]; then
+	tag+="-contrib";
+	with_contrib=1;
+fi;
+image=maximvdw/opencv-nodejs:$tag
+echo building $image
+docker build -t $image --build-arg OPENCV_VERSION=$1 --build-arg WITH_CONTRIB=$with_contrib --build-arg NODEJS_MAJOR_VERSION=$3 .
